8e5cb29f5fa95556286a9b29051e2b2c93b30bfc
coderely <120226475+coderely@users.noreply.github.com>
2024-02-10 11:49:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let bgMusic = document.querySelector('#bgMusic');
 const mains = document.querySelector(".mains");
 const imgdeg = document.getElementById("img");
 
-moveButton.addEventListener('mouseover', ()=>{
+moveButton.addEventListener('click', ()=>{
     moveButton.style.left =  randomNumber() + "%";
     moveButton.style.top = randomNumber() + "%";
     moveButton.style.position = "absolute";
@@ -37,7 +37,7 @@ const typingEffect = ()=>{
         if(i < messgae.length){
             document.getElementById('mes').innerHTML+= messgae.charAt(i);
             i++;
-            setTimeout(typeWrite, 200);
+            setTimeout(typeWrite, 50);
         }
     }
     typeWrite();
@@ -45,7 +45,7 @@ const typingEffect = ()=>{
 
 const imgElem = document.querySelector("#img");
 
-const img = ["https://i.pinimg.com/originals/b5/47/6e/b5476e2c5fb1cd6c9ef14a4ee5a1b72a.gif","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwrDTY-EnCSnSiCn4ib-xBAMyFzArxTO2k7g5h-Ii-Mua5CGVHhiWHb3eR419BWm8g3Ac&usqp=CAU","https://i.pinimg.com/originals/7f/93/b4/7f93b4af0a3c753a7ee9cc0fbd2dd3d0.gif","https://i.pinimg.com/originals/ee/d6/e5/eed6e5c09ed199ee93392da5f4a64719.gif","https://i.pinimg.com/originals/58/99/f6/5899f6e93d8a463fb930a3714ceb7151.gif","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlgtFYvJFp9vOdupQ0DRLmQsXeiRA4zND6YroJpwX1zY9a0Avmy0OqDpyTsU-fehGemhE&usqp=CAU"]
+const img = ["https://i.pinimg.com/originals/3d/6d/85/3d6d850aae1bd1a75ecea1b7e5381735.gif","https://i.pinimg.com/originals/b5/47/6e/b5476e2c5fb1cd6c9ef14a4ee5a1b72a.gif","https://i.pinimg.com/originals/7f/93/b4/7f93b4af0a3c753a7ee9cc0fbd2dd3d0.gif","https://i.pinimg.com/originals/58/99/f6/5899f6e93d8a463fb930a3714ceb7151.gif","https://i.pinimg.com/originals/31/15/98/31159803ceb30f8bc31edadccfeffe5f.gif","https://i.pinimg.com/originals/31/15/98/31159803ceb30f8bc31edadccfeffe5f.gif","https://i.pinimg.com/originals/b7/1e/5d/b71e5d43934f51200db961ff0e85ce6c.gif"]
 
 let currentIndex = 0;
 function changeImg(){
